ca8e63c1732c4bc3ec8b31b3efbcd2ed45495a20
Matteo Bilotta <mbilotta@linkgroup.it>
2020-01-14 10:49:41 +0100

    [FIX] Corrected an import name.

--- src/types/plugins.d.ts
+++ src/types/plugins.d.ts
@@ -3,12 +3,12 @@
 //
 
 import Vue from "vue";
-import VueAnimation, { AnimationOptions } from "@byloth/vue-scroll-animator/base/ScrollAnimation";
+import ScrollAnimation, { AnimationOptions } from "@byloth/vue-scroll-animator/base/ScrollAnimation";
 
 declare module "vue/types/vue"
 {
     interface Vue
     {
-        $scrollAnimate(options: AnimationOptions): VueAnimation;
+        $scrollAnimate(options: AnimationOptions): ScrollAnimation;
     }
 }
