f7f937a85cf0fcaf681de7bbc5ccb4539d618262
Matteo Bilotta <me@byloth.net>
2020-01-28 23:53:06 +0100

    [WIP] Working, again, on Issue #1...

--- src/mdc/foundation/DrawerFoundation.ts
+++ src/mdc/foundation/DrawerFoundation.ts
@@ -1,12 +1,34 @@
 import { MDCDrawerAdapter } from "@material/drawer/adapter";
+import { cssClasses } from "@material/drawer/constants";
 
 import MDCModalDrawerFoundation from "@material/drawer/modal/foundation";
 
 export default class DrawerFoundation extends MDCModalDrawerFoundation
 {
+    public static DESKTOP_SIZE: number = 1200;
+    public static TABLET_SIZE: number = 600;
+
     public constructor(adapter: MDCDrawerAdapter)
     {
         super(adapter);
+    }
+
+    protected _setDismissable(): void
+    {
+        this.adapter_.addClass(cssClasses.DISMISSIBLE);
+        this.adapter_.removeClass(cssClasses.MODAL);
+    }
+
+    protected _setModal(): void
+    {
+        this.adapter_.addClass(cssClasses.MODAL);
+        this.adapter_.removeClass(cssClasses.DISMISSIBLE);
+    }
+
+    protected _setPermanent(): void
+    {
+        this.adapter_.removeClass(cssClasses.MODAL);
+        this.adapter_.removeClass(cssClasses.DISMISSIBLE);
     }
 
     public handleWindowResize(evt: Event): void
@@ -15,6 +37,19 @@ export default class DrawerFoundation extends MDCModalDrawerFoundation
          * TODO: https://github.com/Byloth/website/issues/1
          */
 
-        console.debug(evt);
+        const windowWidth = window.innerWidth;
+
+        if (windowWidth >= DrawerFoundation.DESKTOP_SIZE)
+        {
+            this._setPermanent();
+        }
+        else if (windowWidth >= DrawerFoundation.TABLET_SIZE)
+        {
+            this._setDismissable();
+        }
+        else
+        {
+            this._setModal();
+        }
     }
 }
