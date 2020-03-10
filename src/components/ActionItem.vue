7a9ccaa76cccd6a1d49fd6ffe921a6a30461a218
Matteo Bilotta <me@byloth.net>
2020-03-10 22:09:04 +0100

    [WIP] Other things done...

--- src/components/ActionItem.vue
+++ src/components/ActionItem.vue
@@ -2,24 +2,28 @@
     <button class="material-icons mdc-icon-button mdc-ripple-upgraded--unbounded mdc-ripple-upgraded"
             :alt="description"
             :title="description"
-            :aria-label="description">
-        {{ icon }}
+            :aria-label="description"
+            @click="$emit('click', $event)">
+        <slot />
     </button>
 </template>
 
 <script lang="ts">
+    import { MDCRipple } from "@material/ripple";
     import { Component, Prop, Vue } from "vue-property-decorator";
 
     @Component({ name: "ActionItem" })
     export default class ActionItem extends Vue
     {
-        @Prop({
-            required: true,
-            type: String
-        })
-        public icon!: string;
+        protected _ripple!: MDCRipple;
 
         @Prop(String)
         public description?: string;
+
+        public mounted(): void
+        {
+            this._ripple = new MDCRipple(this.$el);
+            this._ripple.unbounded = true;
+        }
     }
 </script>
