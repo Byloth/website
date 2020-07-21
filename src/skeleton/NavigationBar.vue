b2375f77f71c3d5b0f7f67ce8aa51dd6d60cb424
Matteo Bilotta <me@byloth.net>
2020-07-21 16:34:50 +0200

    [FIX] Fixed wrong behaviour.

--- src/skeleton/NavigationBar.vue
+++ src/skeleton/NavigationBar.vue
@@ -132,7 +132,7 @@
 
         &.mdc-top-app-bar--prominent > .mdc-top-app-bar__row
         {
-            height: 192px;
+            height: 100%;
 
             .mdc-top-app-bar__title
             {
