8958ac2ebf14b673b6c761ed3d2c37614d948d5b
Matteo Bilotta <mbilotta@linkeurope.it>
2020-09-18 15:17:02 +0200

    [IMP] Added SASS. + Added routes.

---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+    <main id="about-page">
+        <h1>This is the about page.</h1>
+    </main>
+</template>
+
+<script lang="ts">
+    import Vue from "vue";
+
+    export default Vue.extend({ name: "AboutPage" });
+</script>
---
+++ pages/blog/_slug.vue
@@ -0,0 +1,11 @@
+<template>
+    <main id="article-page">
+        <h1>This is an article page.</h1>
+    </main>
+</template>
+
+<script lang="ts">
+    import Vue from "vue";
+
+    export default Vue.extend({ name: "ArticlePage" });
+</script>
