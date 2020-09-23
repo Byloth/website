1489de33b62b3ca8bcdbc20beaf052ebb79541f2
Matteo Bilotta <mbilotta@linkeurope.it>
2020-09-23 10:10:39 +0200

    [FIX] Minor fixes.

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,7 @@ export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: "Byloth's Website",
+    titleTemplate: "%s | Byloth's Website",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
