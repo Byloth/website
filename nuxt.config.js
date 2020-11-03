bdccfd1df55e805d322224a643cabc691c6fb6dd
Matteo Bilotta <me@byloth.net>
2020-11-03 10:03:27 +0100

    [UPD] Updated dependencies. 💉💊

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: "%s 🡒 Byloth's Website",
+    titleTemplate: "%s @ Byloth's Website",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
