f7dc2cb07d61b83a83607bad3bb06f533294346b
Matteo Bilotta <mbilotta@linkeurope.it>
2021-02-01 15:34:46 +0100

    [ADD] Added 'send-message' API.

---
+++ static/api/send-message.php
@@ -0,0 +1,95 @@
+<?php
+    function json_response($responseCode = 204, $responseData = null)
+    {
+        http_response_code($responseCode);
+
+        die(json_encode($responseData));
+    }
+
+    if (!isSet($_SERVER["RECIPIENT"]))
+    {
+        json_response(500, [
+            "type" => "env_error_recipient",
+            "text" => "Sorry... An error has occurred. Please, try contact me in some other way."
+        ]);
+    }
+    $recipient = $_SERVER["RECIPIENT"];
+
+    if (!isSet($_SERVER["SENDER"]))
+    {
+        json_response(500, [
+            "type" => "env_error_sender",
+            "text" => "Sorry... An error has occurred. Please, try contact me in some other way."
+        ]);
+    }
+    $sender = $_SERVER["SENDER"];
+
+    if (empty($_POST))
+    {
+        json_response(400, [
+            "type" => "empty_request",
+            "text" => "You request is empty. It must be in AJAX POST format."
+        ]);
+    }
+    if ((!isSet($_SERVER["HTTP_X_REQUESTED_WITH"])) and (strToLower($_SERVER["HTTP_X_REQUESTED_WITH"]) != "xmlhttprequest"))
+    {
+        json_response(400, [
+            "type" => "invalid_format",
+            "text" => "The format of your request isn't valid. It must must be in AJAX POST format."
+        ]);
+    }
+
+    $name = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    $subject =  filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
+    $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+    {
+        json_response(400, [
+            "type" => "invalid_email",
+            "text" => "The e-mail address you wrote is invalid. Please, enter a valid one."
+        ]);
+    }
+
+    if (!trim($message))
+    {
+        json_response(400, [
+            "type" => "empty_message",
+            "text" => "The message you wrote is empty. Please, enter a message before sending it."
+        ]);
+    }
+
+    $boundary = "=_NextPart_" . md5(uniqid(time()));
+
+    $headers = null;
+    $headers .= "From: " . $sender . "\r\n";
+    $headers .= "Reply-To: \"" . $name . "\" <" . $email . ">\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: multipart/alternative;\n\tboundary=\"" . $boundary . "\"\r\n";
+    $headers .= "X-Mailer: PHP " . phpversion();
+
+    $message_body = null;
+    $message_body .= "This is a multi-part message in MIME format.\r\n\r\n";
+    $message_body .= "--" . $boundary . "\r\n";
+    $message_body .= "Content-Type: text/plain;\n\tcharset=\"utf-8\"\r\n";
+    $message_body .= "Content-Transfer-Encoding: 8bit\r\n\r\n";
+    $message_body .= $message . "\r\n\r\n";
+    $message_body .= "\r\n--" . $boundary . "--\r\n";
+
+    $hasSent = mail($recipient, $subject, $message_body, $headers);
+
+    if ($hasSent)
+    {
+        json_response(200, [
+            "type" => "success",
+            "text" => "Thanks " . $name . " for your message! I'll get back to you as soon as possible."
+        ]);
+    }
+    else
+    {
+        json_response(500, [
+            "type" => "unknown_error",
+            "text" => "Sorry... An unexpected and unknown error has occurred."
+        ]);
+    }
