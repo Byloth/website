93f7519441ba63b2e403cd9b7d01a94a84e60c1b
Matteo Bilotta <me@byloth.net>
2023-01-23 18:04:21 +0100

    [IMP] Implemented the scroll separation between sidebar & container.

--- .vitepress/theme/layouts/DoubleCols.vue
+++ .vitepress/theme/layouts/DoubleCols.vue
@@ -2,39 +2,60 @@
 </script>
 
 <template>
-    <div id="layout--double-cols">
-        <div class="layout--double-cols__col sidebar"></div>
-        <div class="layout--double-cols__col container"></div>
-    </div>
+    <div class="sidebar"></div>
+    <div class="container"></div>
 </template>
 
 <style lang="scss" scoped>
-    #layout--double-cols
-    {
-        display: flex;
-    }
-
-    .layout--double-cols__col
-    {
-        height: 100vh;
-        margin: unset;
-    }
+    $container-size: 21cm;
+    $sidebar-size: 250px;
 
     .sidebar
     {
         background-color: rgba(0, 0, 0, 0.25);
 
-        $size: calc(21cm + 250px);
-        $offset: 125px;
+        width: #{$sidebar-size};
+        height: 100vh;
 
-        margin-left: max(((100% - #{$size}) / 2) - #{$offset}, (100% - #{$size}) / 2.5);
+        position: fixed;
+        top: 0px;
+        left: 0px;
 
-        width: 250px;
+        @media only screen and (max-width: 1059px)
+        {
+            @keyframes open-close
+            {
+                0% { margin-left: 0px; }
+                50% { margin-left: -#{$sidebar-size}; }
+                100% { margin-left: 0px; }
+            }
+
+            animation: open-close 2.5s ease-in-out infinite;
+        }
+        @media only screen and (min-width: 1060px)
+        {
+            $size: calc($container-size + $sidebar-size);
+
+            margin-left: max(((100% - #{$size}) / 2) - 125px, (100% - #{$size}) / 2.5);
+        }
     }
     .container
     {
         background-color: rgba(255, 255, 255, 0.75);
 
-        width: 21cm;
+        width: #{$container-size};
+        height: 200vh;
+
+        @media only screen and (max-width: 1059px)
+        {
+            margin: auto;
+        }
+        @media only screen and (min-width: 1060px)
+        {
+            $size: calc($container-size + $sidebar-size);
+            $offset: $sidebar-size;
+
+            margin-left: max(((100% - #{$size}) / 2) + (#{$offset} - 125px), ((100% - #{$size}) / 2.5) + #{$offset});
+        }
     }
 </style>
