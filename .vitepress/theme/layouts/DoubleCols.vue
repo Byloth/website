85dcf877a51bddda29fe1ace4af8373dfabee216
Matteo Bilotta <me@byloth.net>
2023-01-23 17:00:42 +0100

    [FIX] Better debug CSS rules. + Polished CSS rules for 2cols layout.

--- .vitepress/theme/layouts/DoubleCols.vue
+++ .vitepress/theme/layouts/DoubleCols.vue
@@ -3,98 +3,38 @@
 
 <template>
     <div id="layout--double-cols">
-        <div class="__debug layout--double-cols__col sidebar"></div>
-        <div class="__debug layout--double-cols__col container"></div>
+        <div class="layout--double-cols__col sidebar"></div>
+        <div class="layout--double-cols__col container"></div>
     </div>
 </template>
 
 <style lang="scss" scoped>
     #layout--double-cols
     {
-        background-color: #FFFFFF;
         display: flex;
-        padding: 0.5rem 1rem;
     }
 
     .layout--double-cols__col
     {
-        background-color: rgba(0, 0, 0, 0.1);
-        height: calc(100vh - 2rem - 2px);
+        height: 100vh;
         margin: unset;
     }
 
     .sidebar
     {
-        width: 250px;
+        background-color: rgba(0, 0, 0, 0.25);
 
         $size: calc(21cm + 250px);
         $offset: 125px;
 
-        margin-left: max(
-            calc(((100% - #{$size}) / 2) - #{$offset} - (2rem - 4px)),
-            calc(((100% - #{$size}) / 2.5) - (2rem - 4px)),
-            0px
-        );
+        margin-left: max(((100% - #{$size}) / 2) - #{$offset}, (100% - #{$size}) / 2.5);
+
+        width: 250px;
     }
     .container
     {
+        background-color: rgba(255, 255, 255, 0.75);
+
         width: 21cm;
-    }
-
-    /* XXS */
-
-    @media only screen and (min-width: 320px)
-    {
-        /* XS */
-
-        .layout--double-cols__col
-        {
-            background-color: rgba(255, 0, 0, 0.1);
-        }
-    }
-    @media only screen and (min-width: 640px)
-    {
-        /* S */
-
-        .layout--double-cols__col
-        {
-            background-color: rgba(255, 255, 0, 0.1);
-        }
-    }
-    @media only screen and (min-width: 960px)
-    {
-        /* M */
-
-        .layout--double-cols__col
-        {
-            background-color: rgba(0, 255, 0, 0.1);
-        }
-    }
-    @media only screen and (min-width: 1280px)
-    {
-        /* L */
-
-        .layout--double-cols__col
-        {
-            background-color: rgba(0, 255, 255, 0.1);
-        }
-    }
-    @media only screen and (min-width: 1600px)
-    {
-        /* XL */
-
-        .layout--double-cols__col
-        {
-            background-color: rgba(0, 0, 255, 0.1);
-        }
-    }
-    @media only screen and (min-width: 1920px)
-    {
-        /* XXL */
-
-        .layout--double-cols__col
-        {
-            background-color: rgba(255, 0, 255, 0.1);
-        }
     }
 </style>
