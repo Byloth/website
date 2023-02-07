76e338f304a95653ad582a11bcbcbb8e5da74b48
Matteo Bilotta <me@byloth.net>
2023-02-07 17:46:43 +0100

    [FIX] Better scrolling animations. 😎

--- .vitepress/theme/layouts/FloatingHeader.vue
+++ .vitepress/theme/layouts/FloatingHeader.vue
@@ -10,64 +10,72 @@
 
     onMounted(() =>
     {
-        if (!header.value)
-        {
-            return;
-        }
-
+        if (!header.value) { return; }
         const target = header.value;
 
-        const resizeAnimation = scrollAnimate({
+        scrollAnimate({
             target: target,
+            mediaQuery: "screen and (max-width: 599px)",
+            startValue: 0,
+            endValue: 136,
+            classes: [{
+                names: ["shadowed"],
+                threshold: AnimatorThreshold.FromEnd
+            }],
+            styles: [{
+                startValue: 192,
+                endValue: 56,
+
+                computeStyle: (value) => ({
+                    backgroundColor: `rgba(255, ${Math.floor(value * MULTIPLIER)}, 0, 0.25)`,
+                    height: `${value}px`
+                })
+            }]
+        });
+        scrollAnimate({
+            target: target,
+            mediaQuery: "screen and (max-width: 599px)",
+            startValue: 137,
+            boundary: 66,
+            styles: [{
+                startValue: 0,
+                endValue: -66,
+
+                computeStyle: (value) => ({ top: `${value}px` })
+            }]
+        });
+
+        scrollAnimate({
+            target: target,
+            mediaQuery: "screen and (min-width: 600px)",
             startValue: 0,
             endValue: 128,
             classes: [{
                 names: ["shadowed"],
                 threshold: AnimatorThreshold.FromEnd
             }],
-            styles: [
-                {
-                    startValue: 192,
-                    endValue: 56,
-                    mediaQuery: "screen and (max-width: 599px)",
+            styles: [{
+                startValue: 192,
+                endValue: 64,
+                mediaQuery: "screen and (min-width: 600px)",
 
-                    computeStyle: (value) => ({
-                        backgroundColor: `rgba(255, ${Math.floor(value * MULTIPLIER)}, 0, 0.25)`,
-                        height: `${value}px`
-                    })
-                },
-                {
-                    startValue: 192,
-                    endValue: 64,
-                    mediaQuery: "screen and (min-width: 600px)",
-
-                    computeStyle: (value) => ({
-                        backgroundColor: `rgba(0, ${Math.floor(value * MULTIPLIER)}, 255, 0.25)`,
-                        height: `${value}px`
-                    })
-                }
-            ]
+                computeStyle: (value) => ({
+                    backgroundColor: `rgba(0, ${Math.floor(value * MULTIPLIER)}, 255, 0.25)`,
+                    height: `${value}px`
+                })
+            }]
         });
-        const moveAnimation = scrollAnimate({
+        scrollAnimate({
             target: target,
+            mediaQuery: "screen and (min-width: 600px)",
             startValue: 129,
             boundary: 74,
-            styles: [
-                {
-                    startValue: 0,
-                    endValue: -66,
-                    mediaQuery: "screen and (max-width: 599px)",
+            styles: [{
+                startValue: 0,
+                endValue: -74,
 
-                    computeStyle: (value) => ({ top: `${value}px` })
-                },
-                {
-                    startValue: 0,
-                    endValue: -74,
-                    mediaQuery: "screen and (min-width: 600px)",
-
-                    computeStyle: (value) => ({ top: `${value}px` })
-                }
-            ]
+                computeStyle: (value) => ({ top: `${value}px` })
+            }]
         });
     });
 </script>
