12013b3611a19dfc0584386b5f6121cfa2d76827
Matteo Bilotta <me@byloth.net>
2023-06-19 17:59:35 +0200

    [UPD] Updated dependencies. 💊💉

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -4,13 +4,24 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
   lang: "it-IT",
   title: "Byloth's Website",
-  description: "My personal static-generated website; based on Vue.js & VitePress.",
+  description: "My personal static-generated website; based on Vue.js & VitePress. 🌐",
 
   lastUpdated: true,
 
   srcDir: "./src",
+  cacheDir: "./cache",
   outDir: "./dist",
 
+  themeConfig: {
+    footer: {
+      message: `Rilasciato sotto
+<a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank" rel="noopener noreferrer nofollow">
+  Licenza CC BY-SA 4.0</a>.`,
+      copyright: `Copyright © 2014-${new Date().getFullYear()}
+<a href="https://github.com/Byloth">
+  Matteo Bilotta</a>.`
+    }
+  },
   vite: {
     resolve: {
       alias: { "@": resolve(__dirname, "../src") }
